289a5214df5fd81f9c0781a8b7acd97847fabd63
Jose Oliveira <josemiguelpinheiro@gmail.com>
2023-07-24 23:03:19 +0100

    Updated main.js

--- js/main.js
+++ js/main.js
@@ -6,5 +6,48 @@ document.addEventListener('DOMContentLoaded', function() {
       const poke2 = document.querySelector('#poke2').value;
       const url = `https://pokeapi.co/api/v2/pokemon/${poke1}`;
       const url2 = `https://pokeapi.co/api/v2/pokemon/${poke2}`;
+    
+      let pokeStore = [];
+      let pokeImg = [];
+  
+      fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+          pokeStore.push(data.types[0].type.name);
+          pokeImg.push(data.sprites.front_shiny);
+  
+          fetch(url2)
+            .then(res => res.json()) // parse response as JSON
+            .then(data => {
+              pokeStore.push(data.types[0].type.name);
+              pokeImg.push(data.sprites.front_shiny);
+  
+              if (
+                (pokeStore[0] === 'grass' && pokeStore[1] === 'water') ||
+                (pokeStore[0] === 'water' && pokeStore[1] === 'fire') ||
+                (pokeStore[0] === 'fire' && pokeStore[1] === 'grass')
+              ) {
+                document.querySelector('#pokeImg1').src = pokeImg[0];
+                document.querySelector('#pokeImg2').src = pokeImg[1];
+                document.querySelector('h2').innerText = ' 2x > ';
+              }
+            })
+            .catch(err => {
+              console.log(`error ${err}`);
+            });
+        })
+        .catch(err => {
+          console.log(`error ${err}`);
+        });
     }
-};
+  
+    // Adding event listeners to all elements with class 'inputValue'
+    const inputValues = document.querySelectorAll('.inputValue');
+    inputValues.forEach(inputValue => {
+      inputValue.addEventListener('keypress', function(event) {
+        if (event.key === 'Enter') {
+          getFetch();
+        }
+      });
+    });
+  });
