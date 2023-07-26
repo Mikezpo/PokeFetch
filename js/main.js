8d8e27fc9d553d79911b2380c8c27dff15b76330
Jose Oliveira <josemiguelpinheiro@gmail.com>
2023-07-26 12:25:01 +0100

    Updated main.js

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
             pokeImg.push(data.sprites.front_shiny);
 
-            // Check both possible type combinations for each Pokémon
+            // Check type 2 for each Pokémon
             if (
               (pokeStore[0] === 'grass' && (pokeStore[2] === 'water' || pokeStore[2] === 'ground' || pokeStore[2] === 'rock')) ||
               (pokeStore[0] === 'water' && (pokeStore[2] === 'fire' || pokeStore[2] === 'ground' || pokeStore[2] === 'rock')) ||
@@ -50,15 +50,18 @@ document.addEventListener('DOMContentLoaded', function() {
               (pokeStore[0] === 'psychic' && (pokeStore[2] === 'fighting' || pokeStore[2] === 'poison')) ||
               (pokeStore[0] === 'bug' && (pokeStore[2] === 'grass' || pokeStore[2] === 'psychic' || pokeStore[2] === 'dark')) ||
               (pokeStore[0] === 'rock' && (pokeStore[2] === 'fire' || pokeStore[2] === 'ice' || pokeStore[2] === 'flying' || pokeStore[2] === 'bug')) ||
-              (pokeStore[0] === 'ghost' && pokeStore[2] === 'psychic')
+              (pokeStore[0] === 'ghost' && pokeStore[2] === 'psychic') ||
+              (pokeStore[0] === 'dark' && (pokeStore[2] === 'psychic' || pokeStore[2] === 'ghost')) ||
+              (pokeStore[0] === 'steel' && (pokeStore[2] === 'ice' || pokeStore[2] === 'rock' || pokeStore[2] === 'fairy')) ||
+              (pokeStore[0] === 'fairy' && (pokeStore[2] === 'fighting' || pokeStore[2] === 'dragon' || pokeStore[2] === 'dark'))
 
 
             ) {
               document.querySelector('#pokeImg1').src = pokeImg[0];
               document.querySelector('#pokeImg2').src = pokeImg[1];
-              document.querySelector('h2').innerText = ' 2x > ';
+              document.querySelector('h2').innerText = 'Super effective - 200% -->';
 
-
+            // Check type 1 for each Pokémon
             } else if (
               (pokeStore[0] === 'grass' && (pokeStore[1] === 'water' || pokeStore[1] === 'ground' || pokeStore[1] === 'rock')) ||
               (pokeStore[0] === 'water' && (pokeStore[1] === 'fire' || pokeStore[1] === 'ground' || pokeStore[1] === 'rock')) ||
@@ -72,14 +75,17 @@ document.addEventListener('DOMContentLoaded', function() {
               (pokeStore[0] === 'psychic' && (pokeStore[1] === 'fighting' || pokeStore[1] === 'poison')) ||
               (pokeStore[0] === 'flying' && (pokeStore[1] === 'grass' || pokeStore[1] === 'fighting' || pokeStore[1] === 'bug')) ||
               (pokeStore[0] === 'rock' && (pokeStore[1] === 'fire' || pokeStore[1] === 'ice' || pokeStore[1] === 'flying' || pokeStore[1] === 'bug')) ||
-              (pokeStore[0] === 'ghost' && pokeStore[1] === 'psychic')
+              (pokeStore[0] === 'ghost' && pokeStore[1] === 'psychic') ||
+              (pokeStore[0] === 'dark' && (pokeStore[1] === 'psychic' || pokeStore[1] === 'ghost')) ||
+              (pokeStore[0] === 'steel' && (pokeStore[1] === 'ice' || pokeStore[1] === 'rock' || pokeStore[1] === 'fairy')) ||
+              (pokeStore[0] === 'fairy' && (pokeStore[2] === 'fighting' || pokeStore[2] === 'dragon' || pokeStore[2] === 'dark'))
 
 
 
             ) {
               document.querySelector('#pokeImg1').src = pokeImg[0];
               document.querySelector('#pokeImg2').src = pokeImg[1];
-              document.querySelector('h2').innerText = ' 2x > ';
+              document.querySelector('h2').innerText = 'Super effective - 200% -->';
 
               // Check if Pokemon are the same type
             } else if(
@@ -100,7 +106,7 @@ document.addEventListener('DOMContentLoaded', function() {
             ) {
               document.querySelector('#pokeImg1').src = pokeImg[0];
               document.querySelector('#pokeImg2').src = pokeImg[1];
-              document.querySelector('h2').innerText = '=';
+              document.querySelector('h2').innerText = 'Normal damage - 100%';
             
             } else if(
               (pokeStore[0] === 'grass' && pokeStore[1] === 'grass') ||
@@ -120,32 +126,35 @@ document.addEventListener('DOMContentLoaded', function() {
             ) {
               document.querySelector('#pokeImg1').src = pokeImg[0];
               document.querySelector('#pokeImg2').src = pokeImg[1];
-              document.querySelector('h2').innerText = '=';
+              document.querySelector('h2').innerText = 'Normal damage - 100%';
 
+            // Check if Pokemon has 2x effect on same type
             } else if(
-              (pokeStore[0] === 'ghost' && pokeStore[2] === 'ghost')
+              (pokeStore[0] === 'ghost' && pokeStore[2] === 'ghost') ||
+              (pokeStore[0] === 'dragon' && pokeStore[2] === 'dragon')
               
 
             ) {
               document.querySelector('#pokeImg1').src = pokeImg[0];
               document.querySelector('#pokeImg2').src = pokeImg[1];
-              document.querySelector('h2').innerText = '2x < >';
+              document.querySelector('h2').innerText = '<-- Super effective - 200% -->';
 
             
             } else if(
-              (pokeStore[0] === 'ghost' && pokeStore[1] === 'ghost')
+              (pokeStore[0] === 'ghost' && pokeStore[1] === 'ghost') ||
+              (pokeStore[0] === 'dragon' && pokeStore[1] === 'dragon')
               
 
             ) {
               document.querySelector('#pokeImg1').src = pokeImg[0];
               document.querySelector('#pokeImg2').src = pokeImg[1];
-              document.querySelector('h2').innerText = '2x < >';
+              document.querySelector('h2').innerText = '<-- Super effective - 200% -->';
               
               // If none aplies then if has weakness against the other type
             } else {
               document.querySelector('#pokeImg1').src = pokeImg[0];
               document.querySelector('#pokeImg2').src = pokeImg[1];
-              document.querySelector('h2').innerText = ' 2x < ';
+              document.querySelector('h2').innerText = '<-- Super effective - 200%';
             }
           })
           .catch(err => {
