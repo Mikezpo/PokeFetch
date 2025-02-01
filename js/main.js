066a970fbbdf46d2a83d550b030c709ec6ebde1e
Jose Oliveira <josemiguelpinheiro@gmail.com>
2025-02-01 19:04:07 +0000

    Updated code

--- js/main.js
+++ js/main.js
@@ -226,7 +226,8 @@ document.addEventListener('DOMContentLoaded', function () {
     "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte",
     "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno",
     "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo",
-    "Mew",], generation: 1, region: "Kanto" },
+    "Mew"], generation: 1, region: "Kanto" },
+
     //gen 2
     { names: ["Chikorita", "Bayleef", "Meganium", "Cyndaquil", "Quilava", "Typhlosion",
     "Totodile", "Croconaw", "Feraligatr", "Sentret", "Furret", "Hoothoot",
@@ -244,7 +245,8 @@ document.addEventListener('DOMContentLoaded', function () {
     "Kingdra", "Phanpy", "Donphan", "Porygon2", "Stantler", "Smeargle",
     "Tyrogue", "Hitmontop", "Smoochum", "Elekid", "Magby", "Miltank",
     "Blissey", "Raikou", "Entei", "Suicune", "Larvitar", "Pupitar",
-    "Tyranitar", "Lugia", "Ho-Oh", "Celebi",], generation: 2, region: "Johto" },
+    "Tyranitar", "Lugia", "Ho-Oh", "Celebi"], generation: 2, region: "Johto" },
+    
     //gen 3
     { names: ["Treecko", "Grovyle", "Sceptile", "Torchic", "Combusken", "Blaziken",
     "Mudkip", "Marshtomp", "Swampert", "Poochyena", "Mightyena", "Zigzagoon",
@@ -269,6 +271,7 @@ document.addEventListener('DOMContentLoaded', function () {
     "Shelgon", "Salamence", "Beldum", "Metang", "Metagross", "Regirock",
     "Regice", "Registeel", "Latias", "Latios", "Kyogre", "Groudon",
     "Rayquaza", "Jirachi", "Deoxys",], generation: 3, region: "Hoenn" },
+
     //gen 4
     { names: ["Turtwig", "Grotle", "Torterra", "Chimchar", "Monferno", "Infernape",
     "Piplup", "Prinplup", "Empoleon", "Starly", "Staravia", "Staraptor",
@@ -288,6 +291,7 @@ document.addEventListener('DOMContentLoaded', function () {
     "Dusknoir", "Froslass", "Rotom", "Uxie", "Mesprit", "Azelf",
     "Dialga", "Palkia", "Heatran", "Regigigas", "Giratina", "Cresselia",
     "Phione", "Manaphy", "Darkrai", "Shaymin", "Arceus",], generation: 4, region: "Sinnoh" },
+
     //gen 5
     { names: ["Snivy", "Servine", "Serperior", "Tepig", "Pignite", "Emboar",
     "Oshawott", "Dewott", "Samurott", "Patrat", "Watchog", "Lillipup",
@@ -410,64 +414,64 @@ inputValues.forEach(inputValue => {
 // Old code for Pokemon type comparison
 
 // Function to calculate type effectiveness
-// function calculateTypeEffectiveness(pokeTypes) {
-  // Type chart with effectiveness multipliers
-  // const typeChart = {
-  //   normal: { normal: 1, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, fighting: 2, poison: 1, ground: 1, flying: 1, psychic: 1, bug: 1, rock: 1, ghost: 0, dragon: 1, dark: 1, steel: 1, fairy: 1 },
+function calculateTypeEffectiveness(pokeTypes) {
+//  Type chart with effectiveness multipliers
+  const typeChart = {
+    normal: { normal: 1, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, fighting: 2, poison: 1, ground: 1, flying: 1, psychic: 1, bug: 1, rock: 1, ghost: 0, dragon: 1, dark: 1, steel: 1, fairy: 1 },
 
-  //   fire: { normal: 1, fire: 0.5, water: 0.5, electric: 1, grass: 2, ice: 2, fighting: 1, poison: 1, ground: 1, flying: 1, psychic: 1, bug: 2, rock: 0.5, ghost: 1, dragon: 0.5, dark: 1, steel: 2, fairy: 1 },
+    fire: { normal: 1, fire: 0.5, water: 0.5, electric: 1, grass: 2, ice: 2, fighting: 1, poison: 1, ground: 1, flying: 1, psychic: 1, bug: 2, rock: 0.5, ghost: 1, dragon: 0.5, dark: 1, steel: 2, fairy: 1 },
 
-  //   water: { normal: 1, fire: 2, water: 0.5, electric: 1, grass: 0.5, ice: 1, fighting: 1, poison: 1, ground: 2, flying: 1, psychic: 1, bug: 1, rock: 2, ghost: 1, dragon: 0.5, dark: 1, steel: 1, fairy: 1 },
+    water: { normal: 1, fire: 2, water: 0.5, electric: 1, grass: 0.5, ice: 1, fighting: 1, poison: 1, ground: 2, flying: 1, psychic: 1, bug: 1, rock: 2, ghost: 1, dragon: 0.5, dark: 1, steel: 1, fairy: 1 },
 
-  //   electric: { normal: 1, fire: 1, water: 2, electric: 0.5, grass: 0.5, ice: 1, fighting: 1, poison: 1, ground: 0, flying: 2, psychic: 1, bug: 1, rock: 1, ghost: 1, dragon: 0.5, dark: 1, steel: 1, fairy: 1 },
+    electric: { normal: 1, fire: 1, water: 2, electric: 0.5, grass: 0.5, ice: 1, fighting: 1, poison: 1, ground: 0, flying: 2, psychic: 1, bug: 1, rock: 1, ghost: 1, dragon: 0.5, dark: 1, steel: 1, fairy: 1 },
 
-  //   grass: { normal: 1, fire: 0.5, water: 2, electric: 1, grass: 0.5, ice: 1, fighting: 1, poison: 0.5, ground: 2, flying: 0.5, psychic: 1, bug: 0.5, rock: 2, ghost: 1, dragon: 0.5, dark: 1, steel: 0.5, fairy: 1 },
+    grass: { normal: 1, fire: 0.5, water: 2, electric: 1, grass: 0.5, ice: 1, fighting: 1, poison: 0.5, ground: 2, flying: 0.5, psychic: 1, bug: 0.5, rock: 2, ghost: 1, dragon: 0.5, dark: 1, steel: 0.5, fairy: 1 },
 
-  //   ice: { normal: 1, fire: 0.5, water: 0.5, electric: 1, grass: 2, ice: 0.5, fighting: 1, poison: 1, ground: 2, flying: 2, psychic: 1, bug: 1, rock: 1, ghost: 1, dragon: 2, dark: 1, steel: 0.5, fairy: 1 },
+    ice: { normal: 1, fire: 0.5, water: 0.5, electric: 1, grass: 2, ice: 0.5, fighting: 1, poison: 1, ground: 2, flying: 2, psychic: 1, bug: 1, rock: 1, ghost: 1, dragon: 2, dark: 1, steel: 0.5, fairy: 1 },
 
-  //   fighting: { normal: 2, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, fighting: 1, poison: 0.5, ground: 1, flying: 0.5, psychic: 0.5, bug: 0.5, rock: 2, ghost: 0, dragon: 1, dark: 2, steel: 2, fairy: 0.5 },
+    fighting: { normal: 2, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, fighting: 1, poison: 0.5, ground: 1, flying: 0.5, psychic: 0.5, bug: 0.5, rock: 2, ghost: 0, dragon: 1, dark: 2, steel: 2, fairy: 0.5 },
 
-  //   poison: { normal: 1, fire: 1, water: 1, electric: 1, grass: 2, ice: 1, fighting: 1, poison: 0.5, ground: 0.5, flying: 1, psychic: 2, bug: 1, rock: 1, ghost: 0.5, dragon: 1, dark: 1, steel: 0, fairy: 2 },
+    poison: { normal: 1, fire: 1, water: 1, electric: 1, grass: 2, ice: 1, fighting: 1, poison: 0.5, ground: 0.5, flying: 1, psychic: 2, bug: 1, rock: 1, ghost: 0.5, dragon: 1, dark: 1, steel: 0, fairy: 2 },
 
-  //   ground: { normal: 1, fire: 2, water: 1, electric: 2, grass: 0.5, ice: 1, fighting: 1, poison: 2, ground: 1, flying: 0, psychic: 1, bug: 0.5, rock: 2, ghost: 1, dragon: 1, dark: 1, steel: 2, fairy: 1 },
+    ground: { normal: 1, fire: 2, water: 1, electric: 2, grass: 0.5, ice: 1, fighting: 1, poison: 2, ground: 1, flying: 0, psychic: 1, bug: 0.5, rock: 2, ghost: 1, dragon: 1, dark: 1, steel: 2, fairy: 1 },
 
-  //   flying: { normal: 1, fire: 1, water: 1, electric: 0.5, grass: 2, ice: 1, fighting: 2, poison: 1, ground: 1, flying: 1, psychic: 1, bug: 2, rock: 0.5, ghost: 1, dragon: 1, dark: 1, steel: 0.5, fairy: 1 },
+    flying: { normal: 1, fire: 1, water: 1, electric: 0.5, grass: 2, ice: 1, fighting: 2, poison: 1, ground: 1, flying: 1, psychic: 1, bug: 2, rock: 0.5, ghost: 1, dragon: 1, dark: 1, steel: 0.5, fairy: 1 },
 
-  //   psychic: { normal: 1, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, fighting: 2, poison: 2, ground: 1, flying: 1, psychic: 0.5, bug: 1, rock: 1, ghost: 1, dragon: 1, dark: 0, steel: 0.5, fairy: 1 },
+    psychic: { normal: 1, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, fighting: 2, poison: 2, ground: 1, flying: 1, psychic: 0.5, bug: 1, rock: 1, ghost: 1, dragon: 1, dark: 0, steel: 0.5, fairy: 1 },
 
-  //   bug: { normal: 1, fire: 0.5, water: 1, electric: 1, grass: 2, ice: 1, fighting: 0.5, poison: 0.5, ground: 1, flying: 0.5, psychic: 2, bug: 1, rock: 1, ghost: 0.5, dragon: 1, dark: 2, steel: 0.5, fairy: 0.5 },
+    bug: { normal: 1, fire: 0.5, water: 1, electric: 1, grass: 2, ice: 1, fighting: 0.5, poison: 0.5, ground: 1, flying: 0.5, psychic: 2, bug: 1, rock: 1, ghost: 0.5, dragon: 1, dark: 2, steel: 0.5, fairy: 0.5 },
 
-  //   rock: { normal: 1, fire: 2, water: 1, electric: 1, grass: 1, ice: 2, fighting: 0.5, poison: 1, ground: 0.5, flying: 2, psychic: 1, bug: 2, rock: 1, ghost: 1, dragon: 1, dark: 1, steel: 0.5, fairy: 1 },
+    rock: { normal: 1, fire: 2, water: 1, electric: 1, grass: 1, ice: 2, fighting: 0.5, poison: 1, ground: 0.5, flying: 2, psychic: 1, bug: 2, rock: 1, ghost: 1, dragon: 1, dark: 1, steel: 0.5, fairy: 1 },
 
-  //   ghost: { normal: 0, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, fighting: 1, poison: 1, ground: 1, flying: 1, psychic: 1, bug: 1, rock: 1, ghost: 2, dragon: 1, dark: 2, steel: 1, fairy: 1 },
+    ghost: { normal: 0, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, fighting: 1, poison: 1, ground: 1, flying: 1, psychic: 1, bug: 1, rock: 1, ghost: 2, dragon: 1, dark: 2, steel: 1, fairy: 1 },
 
-  //   dragon: { normal: 1, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, fighting: 1, poison: 1, ground: 1, flying: 1, psychic: 1, bug: 1, rock: 1, ghost: 1, dragon: 2, dark: 1, steel: 0.5, fairy: 2 },
+    dragon: { normal: 1, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, fighting: 1, poison: 1, ground: 1, flying: 1, psychic: 1, bug: 1, rock: 1, ghost: 1, dragon: 2, dark: 1, steel: 0.5, fairy: 2 },
 
-  //   dark: { normal: 1, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, fighting: 0.5, poison: 1, ground: 1, flying: 1, psychic: 2, bug: 1, rock: 1, ghost: 2, dragon: 1, dark: 0.5, steel: 1, fairy: 0.5 },
+    dark: { normal: 1, fire: 1, water: 1, electric: 1, grass: 1, ice: 1, fighting: 0.5, poison: 1, ground: 1, flying: 1, psychic: 2, bug: 1, rock: 1, ghost: 2, dragon: 1, dark: 0.5, steel: 1, fairy: 0.5 },
 
-  //   steel: { normal: 1, fire: 0.5, water: 0.5, electric: 0.5, grass: 1, ice: 2, fighting: 1, poison: 1, ground: 1, flying: 1, psychic: 1, bug: 1, rock: 2, ghost: 1, dragon: 1, dark: 1, steel: 0.5, fairy: 2 },
+    steel: { normal: 1, fire: 0.5, water: 0.5, electric: 0.5, grass: 1, ice: 2, fighting: 1, poison: 1, ground: 1, flying: 1, psychic: 1, bug: 1, rock: 2, ghost: 1, dragon: 1, dark: 1, steel: 0.5, fairy: 2 },
 
-  //   fairy: { normal: 1, fire: 0.5, water: 1, electric: 1, grass: 1, ice: 1, fighting: 2, poison: 0.5, ground: 1, flying: 1, psychic: 1, bug: 1, rock: 1, ghost: 1, dragon: 0, dark: 2, steel: 0.5, fairy: 1 },
-  // };
+    fairy: { normal: 1, fire: 0.5, water: 1, electric: 1, grass: 1, ice: 1, fighting: 2, poison: 0.5, ground: 1, flying: 1, psychic: 1, bug: 1, rock: 1, ghost: 1, dragon: 0, dark: 2, steel: 0.5, fairy: 1 },
+  };
 
-  // Calculate effectiveness
-  // let effectiveness = 1;
-  // for (let i = 0; i < pokeTypes[0].length; i++) {
-  //   for (let j = 0; j < pokeTypes[1].length; j++) {
-  //     const attackingType = pokeTypes[0][i];
-  //     const defendingType = pokeTypes[1][j];
-  //     effectiveness *= typeChart[attackingType][defendingType];
-  //   }
-  // }
+//  Calculate effectiveness
+  let effectiveness = 1;
+  for (let i = 0; i < pokeTypes[0].length; i++) {
+    for (let j = 0; j < pokeTypes[1].length; j++) {
+      const attackingType = pokeTypes[0][i];
+      const defendingType = pokeTypes[1][j];
+      effectiveness *= typeChart[attackingType][defendingType];
+    }
+  }
 
-  // Determine the result message based on effectiveness
-//   if (effectiveness === 0) {
-//     return 'No effect - 0% damage';
-//   } else if (effectiveness < 1) {
-//     return 'Not very effective - 50% damage';
-//   } else if (effectiveness === 1) {
-//     return 'Normal - 100% damage';
-//   } else if (effectiveness > 1) {
-//     return 'Super effective - 200% damage';
-//   }
-// }
+//  Determine the result message based on effectiveness
+  if (effectiveness === 0) {
+    return 'No effect - 0% damage';
+  } else if (effectiveness < 1) {
+    return 'Not very effective - 50% damage';
+  } else if (effectiveness === 1) {
+    return 'Normal - 100% damage';
+  } else if (effectiveness > 1) {
+    return 'Super effective - 200% damage';
+  }
+}
